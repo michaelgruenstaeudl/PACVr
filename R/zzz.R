e44c7feff00efc61f128e1b3dd5a8c0d78e69004
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2019-06-12 17:17:20 +0200

    2019.06.12.1700

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,5 @@
+
+
 #.onAttach = function(libname = find.package("PACViR"), pkgname = "PACViR") {
 #    if (!require("pacman")) install.packages("pacman")
 #    pacman::p_load("RCircos", "optparse")
@@ -7,6 +9,10 @@
 #    pacman::p_load("BiocParallel", "genbankr")
 #}
 #
-#.onLoad = function(libname = find.package("PACViR"), pkgname = "PACViR") {
+
+.onLoad = function(libname = find.package("PACViR"), pkgname = "PACViR") {
 #    pacman::p_load("RCircos", "optparse", "genbankr")
-#}
+
+## The RCircos.Env object must be located in the global namespace.
+assign("RCircos.Env", RCircos::RCircos.Env, .GlobalEnv)
+}
