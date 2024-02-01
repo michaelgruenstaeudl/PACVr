33e684b2387e4c7373df6a2b74a99f60e674d3b7
alephnull7 <alephnull7@users.noreply.github.com>
2024-02-01 03:22:14 +0000

    Updated documentation for release

--- R/quadripartiteOperations.R
+++ R/quadripartiteOperations.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.01.0316"
+#version="2024.02.01.0322"
 
 FilterByKeywords <- function(allRegions, where) {
   # Function to filter list based on genomic keywords
