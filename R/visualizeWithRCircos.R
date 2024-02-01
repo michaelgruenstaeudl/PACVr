7cfdabf091ee6b37e8d78d3c2160acefc8a9bfd5
michaelgruenstaeudl <michaelgruenstaeudl@users.noreply.github.com>
2024-02-01 17:36:02 +0000

    Updated documentation for release

--- R/visualizeWithRCircos.R
+++ R/visualizeWithRCircos.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.01.0322"
+#version="2024.02.01.1736"
 
 visualizeWithRCircos <- function(plotTitle,
                                  genes,
