9abe6cda028be850049e27fe8e02b6aaf20a6de0
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-02-02 16:59:21 -0800

    Updated `README_USAGE` for better use in testing

--- inst/extdata/README_USAGE/Mitochondrial_Genome.R
+++ inst/extdata/README_USAGE/Mitochondrial_Genome.R
@@ -9,6 +9,6 @@ outFile <- paste(tempdir(), "/MG936619_CoverageViz.pdf", sep="")
 #outFile <- "~/MG936619_CoverageViz.pdf"   # on R-Studio for Linux
 
 ## ONLY COVERAGE VALUES, NO REGION INDICATORS ##
-PACVr.complete(gbkFile, bamFile, windowSize=50, logScale=FALSE,
-               threshold=0.5, relative=TRUE, textSize=0.5,
-               output=outFile)
+exitStatus <- PACVr.complete(gbkFile, bamFile, windowSize=50, logScale=FALSE,
+                             threshold=0.5, relative=TRUE, textSize=0.5,
+                             output=outFile)
--- inst/extdata/README_USAGE/Plastid_Genome.R
+++ inst/extdata/README_USAGE/Plastid_Genome.R
@@ -7,18 +7,22 @@ bamFile <- system.file("extdata", "NC_045072/NC_045072_subsampled.bam",
 outFile <- paste(tempdir(), "/NC_045072_CoverageViz.pdf", sep="")
 #outFile <- "../NC_045072_CoverageViz.pdf"  # on R-Studio for Windows
 #outFile <- "~/NC_045072_CoverageViz.pdf"   # on R-Studio for Linux
+exitStatusVec <- c()  # useful for large automated jobs
 
 ## ONLY COVERAGE VALUES, NO REGION INDICATORS ##
-PACVr.complete(gbkFile, bamFile, windowSize=250, logScale=FALSE,
-               threshold=0.5, relative=TRUE, textSize=0.5,
-               output=outFile)
+exitStatus <- PACVr.complete(gbkFile, bamFile, windowSize=250, logScale=FALSE,
+                             threshold=0.5, relative=TRUE, textSize=0.5,
+                             output=outFile)
+exitStatusVec <- c(exitStatusVec, exitStatus)
 
 ## COVERAGE VALUES PLUS REGION INDICATORS ##
-PACVr.complete(gbkFile, bamFile, windowSize=250, logScale=FALSE,
-               threshold=0.5, relative=TRUE, textSize=0.5,
-               regionsCheck=0, output=outFile)
+exitStatus <- PACVr.complete(gbkFile, bamFile, windowSize=250, logScale=FALSE,
+                             threshold=0.5, relative=TRUE, textSize=0.5,
+                             regionsCheck=0, output=outFile)
+exitStatusVec <- c(exitStatusVec, exitStatus)
 
 ## COVERAGE VALUES PLUS REGION INDICATORS PLUS IR SYNTENY LINES ##
-PACVr.complete(gbkFile, bamFile, windowSize=250, logScale=FALSE,
-               threshold=0.5, relative=TRUE, textSize=0.5,
-               regionsCheck=1, output=outFile)
+exitStatus <- PACVr.complete(gbkFile, bamFile, windowSize=250, logScale=FALSE,
+                             threshold=0.5, relative=TRUE, textSize=0.5,
+                             regionsCheck=1, output=outFile)
+exitStatusVec <- c(exitStatusVec, exitStatus)
