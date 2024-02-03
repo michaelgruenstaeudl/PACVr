3eabce264a02f4825c2a81051573a2162fd75874
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-02-02 17:01:08 -0800

    Small changes to testing of `PACVr.read.gb`

--- tests/testthat/test-PACVr.R
+++ tests/testthat/test-PACVr.R
@@ -6,7 +6,7 @@ allFiles <- list.files(extData, recursive = TRUE)
 gbkTestFiles <- allFiles[grepl("(?<!-PACVr)\\.gb$", allFiles, perl = TRUE)]
 
 for (gbkFile in gbkTestFiles) {
-  test_that("successful parsing of `{gbkFile}`", {
+  test_that(paste0("successful parsing of `", gbkFile, "`"), {
     gbkFileFull <- file.path(extData, gbkFile)
     gbkData <- suppressMessages(PACVr.read.gb(gbkFileFull))
     gbkDataDF <- read.gb2DF(gbkData, FALSE)
@@ -14,13 +14,13 @@ for (gbkFile in gbkTestFiles) {
   })
 }
 
-# test Entrez sourced data
+# test Entrez sourced GenBank data
 nucIDs <- c(
   "MG936619"
   )
 for (nucID in nucIDs) {
-  test_that("successful parsing of `{nucID}`", {
-    gbkChar <- rentrez::entrez_fetch(db = "nuccore", 
+  test_that(paste0("successful parsing of `", nucID, "`"), {
+    gbkChar <- rentrez::entrez_fetch(db = "nuccore",
                                      id = nucID, 
                                      rettype = "gb")
     gbkData <- suppressMessages(PACVr.read.gb(gbkChar))
