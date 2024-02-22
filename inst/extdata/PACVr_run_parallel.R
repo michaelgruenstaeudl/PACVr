6b47796c7041e567a70cc1bf2440c10f10b2f7f8
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-02-21 16:53:47 -0800

    In `PACVr_run_parallel.R`, print size of multiprocess tasks

--- inst/extdata/PACVr_run_parallel.R
+++ inst/extdata/PACVr_run_parallel.R
@@ -13,6 +13,7 @@ inFiles <- list.files(path=inDir, pattern='annotated.gb', full.names=TRUE) 	 # U
 # Set up parallel backend to use multiple processors
 cores_avail <- floor((detectCores()-1)/2)  # only using half of available cores
 print(paste("Available cores:", cores_avail))
+print(paste("Jobs to execute:", length(inFiles)))
 cl <- makeCluster(cores_avail, type="FORK", outfile=paste0(inDir, "/", "MPI_runs.log"))
 registerDoParallel(cl)
 
