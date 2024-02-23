4dcad2b4b61485235f4cca0cdb5fe18ab59d8426
alephnull7 <alephnull7@users.noreply.github.com>
2024-02-23 04:19:11 +0000

    Updated documentation for release

--- R/visualizeWithRCircos.R
+++ R/visualizeWithRCircos.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.23.0220"
+#version="2024.02.23.0419"
 
 visualizeWithRCircos <- function(gbkData,
                                  coverage,
