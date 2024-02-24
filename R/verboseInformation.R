a93e9ca59d992db61861daca5f5cef470e5360ed
alephnull7 <alephnull7@users.noreply.github.com>
2024-02-24 23:01:29 +0000

    Update documentation for version

--- R/verboseInformation.R
+++ R/verboseInformation.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.24.2219"
+#version="2024.02.24.2301"
 
 getVerbosePath <- function(sampleName,
                            plotSpecs) {
