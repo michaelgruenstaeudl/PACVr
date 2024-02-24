6e0d863a270ca3d325c2c7a84ac29c3763900069
alephnull7 <alephnull7@users.noreply.github.com>
2024-02-24 23:23:46 +0000

    Update documentation for version

--- R/customizedRCircos.R
+++ R/customizedRCircos.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.24.2301"
+#version="2024.02.24.2323"
 
 
 # The following R functions were taken from the R package RCircos and then modified.
