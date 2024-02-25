a5cd59c9bf1c0141e42701e236763a5259da4fa2
alephnull7 <alephnull7@users.noreply.github.com>
2024-02-25 00:19:27 +0000

    Update documentation for version

--- R/PACVr.R
+++ R/PACVr.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.24.2323"
+#version="2024.02.25.0019"
 
 PACVr.read.gb <- function(gbkFile) {
   gbkRaw <- getGbkRaw(gbkFile)
--- R/quadripartiteOperations.R
+++ R/quadripartiteOperations.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.24.2323"
+#version="2024.02.25.0019"
 
 FilterByKeywords <- function(allRegions, where) {
   # Function to filter list based on genomic keywords
