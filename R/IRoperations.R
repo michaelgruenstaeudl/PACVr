f59d618121e17f1c3750cd05cb716c3a52281470
alephnull7 <alephnull7@users.noreply.github.com>
2024-02-25 03:07:41 +0000

    Update documentation for version

--- R/IRoperations.R
+++ R/IRoperations.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.02.25.0041"
+#version="2024.02.25.0307"
 
 checkIREquality <- function(gbkSeq, regions, dir, sample_name) {
   if ("IRb" %in% regions[, 4] && "IRa" %in% regions[, 4]) {
