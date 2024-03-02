d538b6bf2fe02c354f55a5bcc79880acaedc4ef5
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-03-02 11:38:31 -0800

    Combine `ExtractAllGenes()` and `PACVr.parseGenes()`

--- R/parsingOps.R
+++ R/parsingOps.R
@@ -3,22 +3,6 @@
 #email="m_gruenstaeudl@fhsu.edu"
 #version="2024.03.01.1956"
 
-ExtractAllGenes <- function(gbkSeqFeatures) {
-  # Function to extract gene information from Genbank flatfile data
-  # ARGS:
-  #   gbkData (resulting data frame from parsing read.gb object)
-  # RETURNS:
-  #   genes in data frame format
-  logger::log_info('  Extracting information on genes')
-  gene_L <- read.gbGenes(gbkSeqFeatures)
-  gene_L <- gene_L[, c(1:3, which(colnames(gene_L) == "gene"))]
-  colnames(gene_L) <- c("Chromosome", "chromStart", "chromEnd", "gene")
-  gene_L$Chromosome <- ""
-  gene_L <- gene_L[order(gene_L$chromStart),]
-  row.names(gene_L) <- 1:nrow(gene_L)
-  return(gene_L)
-}
-
 PACVr.gbkData <- function(read.gbData, analysisSpecs) {
   gbkSeqFeatures <- read.gbSeqFeatures(read.gbData, analysisSpecs)
   if (is.null(gbkSeqFeatures)) {
@@ -58,10 +42,19 @@ PACVr.gbkData <- function(read.gbData, analysisSpecs) {
 }
 
 PACVr.parseGenes <- function (gbkSeqFeatures) {
-  # This function parses the genes of a GenBank file
-  logger::log_info('Parsing the different genes')
-  genes <- ExtractAllGenes(gbkSeqFeatures)
-  return(genes)
+  # Function to extract gene information from Genbank flatfile data
+  # ARGS:
+  #   gbkData (resulting data frame from parsing read.gb object)
+  # RETURNS:
+  #   genes in data frame format
+  logger::log_info('  Extracting information on genes')
+  gene_L <- read.gbGenes(gbkSeqFeatures)
+  gene_L <- gene_L[, c(1:3, which(colnames(gene_L) == "gene"))]
+  colnames(gene_L) <- c("Chromosome", "chromStart", "chromEnd", "gene")
+  gene_L$Chromosome <- ""
+  gene_L <- gene_L[order(gene_L$chromStart),]
+  row.names(gene_L) <- 1:nrow(gene_L)
+  return(gene_L)
 }
 
 PACVr.quadripRegions <- function(gbkLengths,
