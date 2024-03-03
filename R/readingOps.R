2d0e2573c21b5e01cef8b2b653a52b2d6030f6be
alephnull7 <alephnull7@users.noreply.github.com>
2024-03-03 05:09:26 +0000

    Update documentation for version

--- R/readingOps.R
+++ R/readingOps.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.03.03.0433"
+#version="2024.03.03.0509"
 
 read.gbSeqFeaturesAdapt <- function(gbkData, analysisSpecs) {
   gbkSeqFeatures <- read.gbSeqFeatures(gbkData,
