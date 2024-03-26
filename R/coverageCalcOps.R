225ded066919981b03dd6fbecad29b091228ace0
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-03-26 15:09:44 -0700

    Default unpartitioned genome summary name

--- R/coverageCalcOps.R
+++ R/coverageCalcOps.R
@@ -261,6 +261,9 @@ getCovDepth <- function(covDataField, regions_name = NULL) {
   }
   covDepth <- covDataField %>%
     calcCovDepth()
+  if (!is.null(regions_name) && regions_name == "Source") {
+    covDepth[regions_name] <- "Unpartitioned"
+  }
   return(covDepth)
 }
 
