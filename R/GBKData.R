079814630345545da031754f39fbd2b539c31b9d
alephnull7 <alephnull7@users.noreply.github.com>
2024-03-27 03:11:27 +0000

    Update documentation for version

--- R/GBKData.R
+++ R/GBKData.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.03.23.2208"
+#version="2024.03.27.0311"
 
 GBKData <- R6Class("GBKData",
   public = list(
