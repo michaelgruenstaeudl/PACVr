3c26875d7ed75ac7c7add30eea55c6d0b0d74e50
michaelgruenstaeudl <michaelgruenstaeudl@users.noreply.github.com>
2024-05-20 18:21:30 +0000

    Update documentation for version

--- R/coverageCalcOps.R
+++ R/coverageCalcOps.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.05.16.1704"
+#version="2024.05.20.1821"
 
 CovCalc <- function(coverageRaw,
                     windowSize = 250,
