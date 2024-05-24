d973d0153b0baf248223a5472019119fccc2290c
alephnull7 <alephnull7@users.noreply.github.com>
2024-05-24 20:40:28 +0000

    Update documentation for version

--- R/visualisationOps.R
+++ R/visualisationOps.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.05.21.2239"
+#version="2024.05.24.2040"
 
 vizWithRCircos <- function(gbkData,
                                  coverage,
