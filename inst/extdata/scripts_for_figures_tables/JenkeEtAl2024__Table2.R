6bac3dc4bc283cfa323feea01cea8675774666e0
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-05-30 16:20:24 -0700

    Update Table 2 to include accurate outlier column

--- inst/extdata/scripts_for_figures_tables/JenkeEtAl2024__Table2.R
+++ inst/extdata/scripts_for_figures_tables/JenkeEtAl2024__Table2.R
@@ -18,32 +18,51 @@ source("PREP_coverage_data_preparation.R")
 
 # TABLE 2 - descriptive statistics for the partitions
 create_table_2 <- function(cov_data) {
-  # prepare source dataframe for rstatix
-  table_2 <-
-    cov_data %>% select(LSC, IRb, SSC, IRa,
-                        coding, noncoding,
-                        E_score,
-                        N_count, IR_mismatches)
-  wrsd_cols <- c(
-    "LSC", "IRb", "SSC", "IRa",
-    "coding", "noncoding"
-  )
-  for (col_name in wrsd_cols) {
-    table_2 <- handle_outliers(table_2, col_name, 3, 1)
-  }
+  # long WRSD table with labeled outliers
+  outlier_label_df <- cov_data %>%
+    select(Accession, LSC, IRb, SSC, IRa, coding, noncoding) %>%
+    pivot_longer(
+      cols = -Accession,
+      names_to = "regions", 
+      values_to = "low_coverage",
+      names_prefix = "original_"
+    ) %>%
+    handle_cov_outliers("regions", "low_coverage", 2)
 
-  # create summary table
-  table_2 <-
-    table_2 %>%
-    rename("E-score" = E_score,
-           Ns = N_count,
-           mismatches = IR_mismatches) %>%
-    rstatix::get_summary_stats(show = c("n", "min", "max", "q1", "q3",
-                                        "median", "mean", "sd")) %>%
-    mutate("NA" = nrow(cov_data) - .$n) %>%
-    select(variable, n, "NA", min, max, q1, q3, median, mean, sd)
+  # summarized outliers
+  outlier_df <- outlier_label_df %>%
+    filter(outlier == TRUE) %>%
+    group_by(regions) %>%
+    summarize(outliers = sum(n())) %>%
+    rename(variable = regions)
+
+  # non-WRSD metrics
+  asym_qual_df <- cov_data %>%
+    select(Accession, N_count, IR_mismatches, E_score) %>%
+    rename(
+      "E-score" = E_score,
+      Ns = N_count,
+      mismatches = IR_mismatches
+    )
+
+  # create summary stats table
+  table_2 <- outlier_label_df %>%
+    filter(outlier == FALSE) %>%
+    pivot_wider(
+      id_cols = Accession,
+      names_from = regions,
+      values_from = low_coverage
+    ) %>%
+    full_join(., asym_qual_df) %>%
+    rstatix::get_summary_stats(
+      show = c("n", "min", "max", "q1", "q3",
+               "median", "mean", "sd")
+    ) %>%
+    full_join(., outlier_df) %>%
+    mutate("NA" = nrow(cov_data) - .$n - ifelse(is.na(outliers), 0, outliers)) %>%
+    select(variable, n, "NA", outliers, min, max, q1, q3, median, mean, sd)
   
-  xtab_2 <- xtable(table_2, digits=c(0,0,0,0,2,2,2,2,2,2,2))
+  xtab_2 <- xtable(table_2, digits=c(0,0,0,0,0,2,2,2,2,2,2,2))
   print(
     xtab_2,
     file = "./images/JenkeEtAl2024_Table_2.tex",
