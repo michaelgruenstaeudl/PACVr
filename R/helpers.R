43a7b638e0505f8129e7199260de0bc841ba0167
michaelgruenstaeudl <michaelgruenstaeudl@users.noreply.github.com>
2024-05-31 04:57:10 +0000

    Update documentation for version

--- R/helpers.R
+++ R/helpers.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env RScript
 #contributors=c("Gregory Smith", "Nils Jenke", "Michael Gruenstaeudl")
 #email="m_gruenstaeudl@fhsu.edu"
-#version="2024.05.31.0123"
+#version="2024.05.31.0457"
 
 HistCol <- function(cov, threshold, relative, logScale) {
   # Function to generate color vector for histogram data
